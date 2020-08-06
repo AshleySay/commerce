3e9f5f392f2be6006a7884713e0e1d593918f04c
Ashley Say <green.ashley.m@gmail.com>
2020-08-05 17:53:20 -0700

    Added Working bidding logic

--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.db.models import Max
 
 from .models import User, Listing, Bids
 
@@ -25,15 +26,19 @@ def newlisting(request):
 
 def listing(request, title):
     listing = Listing.objects.get(title=title)
+    max_bid = Bids.objects.all().filter(bid=Listing.objects.get(title=title)).aggregate(Max("value"))
     if request.method == "POST":
         bid = Bids(
             value=request.POST["bid"],
             bid=Listing.objects.get(title=title)
         )
-        bid.save()
+        if (float(bid.value) > listing.starting_bid) and (float(bid.value) > max_bid["value__max"]):
+            bid.save()
+        else:
+            return HttpResponse("Invalid Bid. Either less then original or less then largest")
     return render(request, "auctions/listing.html",{
         "listing": listing,
-        "bids": Bids.objects.all().filter(bid=Listing.objects.get(title=title))
+        "bids": Bids.objects.all().filter(bid=Listing.objects.get(title=title)),
     })
 
 def login_view(request):
